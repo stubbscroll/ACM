0130650b6b6eece34de10bad514a3d970426fc60
stubbscroll <spaans@pvv.org>
2014-08-02 22:21:00 +0200

    added ipsc 2013 h1

---
+++ IPSC/2013/h.c
@@ -0,0 +1,10 @@
+/* solution for histiaeus, easy input
+   algorithm: view source, look at header and fine meta name="H1 ANSWER"
+   with value "SUBROUTINES" */
+
+#include <stdio.h>
+
+int main() {
+	puts("SUBROUTINES");
+	return 0;
+}
